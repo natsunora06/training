58ea9d7f81177839fdea86fbdd7ad310478f2edf
Natsumi I <natsumii@NatsuminoMacBook-ea.local>
2018-01-05 21:53:48 +0900

    jquery second

---
+++ jquery_1/script.js
@@ -0,0 +1,21 @@
+
+var name , setname ;
+$(function(){
+    $(".mini").css("opacity","0.5").hover(up,down).click(changePhoto);
+});
+function up(){
+    $(this).css("opacity","1.0")
+        .css("cursor","pointer")
+        .css("margin-top","10px").css("margin-bottom","30px");
+}
+function down(){
+    $(this).css("opacity","0.5")
+        .css("margin-top","20px").css("margin-bottom","20px");
+}
+
+function changePhoto(){
+    fname = $(this).attr("src");
+    var array = fname.split('/');
+    setname = array[0] + "/b_" + array[1];
+    $("#bigPic").attr("src",setname);
+}
