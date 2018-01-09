2ddb41a194ac1db5accedfad0ef7162993ab988d
Natsumi I <natsumii@NatsuminoMacBook-ea.local>
2018-01-09 10:29:34 +0900

    jqueryカーソルを合わせて写真表示sample2

---
+++ java_1/src/bin/Operator.java
@@ -0,0 +1,26 @@
+package bin;
+
+public class Operator {
+	
+	public static void main(String[] args) {
+		
+		// かつ
+		int age = 16;
+		int b = 1;
+		System.out.println(age <= 18 || age >= 15);
+//		b = 2;
+//		System.out.println(a == b);
+		
+//		// 演算子
+//		a = 1;
+//		b = 2;
+//		System.out.println(a > b);
+//		System.out.println(a < b);
+//		
+//		a = 2;
+//		b = 1;
+//		System.out.println(a >= b);
+//		System.out.println(a <= b);
+	}
+
+}
--- jquery_1/sample.js
+++ jquery_1/sample.js
@@ -1,7 +1,7 @@
 
 var name , setname ;
 $(function(){
-    $(".mini").css("opacity","0.5").hover(up,down).click(changePhoto);
+    $(".mini").css("opacity","0.5").hover(up,down).hover(changePhoto);
 });
 function up(){
     $(this).css("opacity","1.0")
@@ -13,13 +13,19 @@ function down(){
         .css("margin-top","20px").css("margin-bottom","20px");
 }
 
-function changePhoto(){
+function changePhoto() {
     fname = $(this).attr("src");
     // frame = img/pic2.jpg
     var array = fname.split('/');
     // ["img", "pic2.jpg"]
     setname = array[0] + "/b_" + array[1];
     // img + /b_ + pic2.jpg
-    $("#bigPic").attr("src",setname);
+    $("#bigPic").attr("src", setname);
     // setname = "img/b_pic2.jpg"
+}
+function changingPhoto() {
+    $('#bigPic').fadeIn();
+}
+function changingPhoto(){
+    $('#bigPic').fadeOut();
 }
